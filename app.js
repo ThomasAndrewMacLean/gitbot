b4728027caa756dd2af9e65fc32150965ce30b37
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-09 21:16:14 +0200

    created claudia api

---
+++ app.js
@@ -0,0 +1,12 @@
+var ApiBuilder = require('claudia-api-builder'),
+  api = new ApiBuilder();
+
+module.exports = api;
+
+api.get('/hello', function () {
+  return 'hello world';
+});
+
+api.post('/webhook', function () {
+    return 'hello world';
+  });

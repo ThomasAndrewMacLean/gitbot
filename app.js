fb0e671a5852fabb5f1b493ef7bfb13e3fc3ddf4
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-10 00:19:12 +0200

    test husky

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const ApiBuilder = require('claudia-api-builder');
 const AWS = require('aws-sdk');
-//AWS.config.update({ region: 'eu-west-1' });
 
 const api = new ApiBuilder();
 const SES = new AWS.SES();
@@ -14,10 +13,14 @@ api.get('/ping', function() {
 });
 
 api.post('/webhook', function(req) {
-    // if (req.body.action !== 'opened') {
-    console.log(`this is a ${req.body.action}not an PR... will shut down`);
-    // return;
-    //}
+    if (req.body.action !== 'opened') {
+        console.log(`this is a ${req.body.action}, not an PR... will shut down`);
+        return;
+    }
+
+    
+
+
 
     let msg = '';
     for (const key in req.body) {
@@ -29,22 +32,6 @@ api.post('/webhook', function(req) {
         Destination: { ToAddresses: [recipient] },
         Message: { Subject: { Data: subject }, Body: { Text: { Data: msg } } }
     };
-
-    console.log('OK123');
-
-    // SES.sendEmail(email, (err, data) => {
-    //     if (err) console.log(err, err.stack);
-    //     else console.log(data);
-    // });
-    // const sendPromise = new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(email).promise();
-
-    // sendPromise
-    //     .then(function(data) {
-    //         console.log(data.MessageId);
-    //     })
-    //     .catch(function(err) {
-    //         console.error(err, err.stack);
-    //     });
 
     return SES.sendEmail(email)
         .promise()

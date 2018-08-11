32e2072631bdca78530298281a6a33539453fd86
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-11 11:26:45 +0200

    test promis all

--- app.js
+++ app.js
@@ -22,6 +22,67 @@ const verify = req => {
     return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(computedSignature));
 };
 
+const sendEmail = req => {
+    let msg = '';
+    for (const key in req.body) {
+        msg += key + ': ' + JSON.stringify(req.body[key]) + '\n';
+    }
+
+    const email = {
+        Source: sender,
+        Destination: { ToAddresses: [recipient] },
+        Message: { Subject: { Data: subject }, Body: { Text: { Data: msg } } }
+    };
+    return SES.sendEmail(email)
+        .promise()
+        .then(() => {
+            console.log('EMAIL SENT');
+        })
+        .catch(err => {
+            console.log('ERROR: EMAIL NOT SENT');
+            console.log(err);
+        });
+};
+
+const setLabels = PR => {
+    return fetch(PR.issue_url + '/labels', {
+        method: 'POST',
+        body: JSON.stringify(['bug', 'question']),
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + process.env.GITHUBTOKEN
+        }
+    })
+        .then(() => {
+            console.log('SET LABELS');
+        })
+        .catch(err => {
+            console.log('ERROR: LABELS NOT SET');
+            console.log(err);
+        });
+};
+
+const setBody = PR => {
+    PR.body = PR.body.replace('test', 'production');
+
+    return fetch(PR.url, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            body: PR.body + '\n\n\n\n I can append this to the original body'
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + process.env.githubToken
+        }
+    })
+        .then(() => {
+            console.log('SET BODY');
+        })
+        .catch(err => {
+            console.log('ERROR: BODY NOT SET');
+            console.log(err);
+        });
+};
 api.get('/ping', () => {
     return 'pong';
 });
@@ -37,52 +98,7 @@ api.post('/webhook', req => {
     }
 
     const PR = req.body.pull_request;
-
-    let msg = '';
-    for (const key in req.body) {
-        msg += key + ': ' + JSON.stringify(req.body[key]) + '\n';
-    }
-
-    const email = {
-        Source: sender,
-        Destination: { ToAddresses: [recipient] },
-        Message: { Subject: { Data: subject }, Body: { Text: { Data: msg } } }
-    };
-
-    return SES.sendEmail(email)
-        .promise()
-        .then(function() {
-            return fetch(PR.issue_url + '/labels', {
-                method: 'POST',
-                body: JSON.stringify(['bug', 'question']),
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: 'Bearer ' + process.env.GITHUBTOKEN
-                }
-            })
-                .then(d => {
-                    console.log(d);
-                    PR.body = PR.body.replace('test', 'production');
-
-                    return fetch(PR.url, {
-                        method: 'PATCH',
-                        body: JSON.stringify({
-                            body: PR.body + '\n\n\n\n I can append this to the original body'
-                        }),
-                        headers: {
-                            'Content-Type': 'application/json',
-                            Authorization: 'Bearer ' + process.env.githubToken
-                        }
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        })
-        .catch(function(err) {
-            console.log('Error sending mail: ' + err);
-            return { status: 'ERROR' };
-        });
+    return Promise.all([sendEmail(req), setLabels(PR), setBody(PR)]);
 });
 
 module.exports = api;

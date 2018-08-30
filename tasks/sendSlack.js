3c58c787bae39d0ce3c0254fe5b0d426f83b4764
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-31 00:30:45 +0200

    hardcode url

--- tasks/sendSlack.js
+++ tasks/sendSlack.js
@@ -1,40 +1,34 @@
 const fetch = require('node-fetch');
-const db = require('monk')(
-    `mongodb://dbreadwrite:${
-        process.env.MONGO_PW
-    }@ds139992.mlab.com:39992/gitbot`
-);
-const users = db.get('users');
+// const db = require('monk')(
+//     `mongodb://dbreadwrite:${
+//         process.env.MONGO_PW
+//     }@ds139992.mlab.com:39992/gitbot`
+// );
+//const users = db.get('users');
 
 const sendSlack = PR => {
     console.log('GETTING SLACK 🤖');
 
     //GET SLACKURL FROM PR
-    users
-        .find({
-            email: 'thomas.maclean@marlon.be'
+
+    return fetch(
+        'https://hooks.slack.com/services/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',
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                text: `new pull request to be reviewed!!! PR: ${PR.title}`
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+        .then(() => {
+            console.log('SEND SLACK');
         })
-        .then(user => {
-            console.log('USEEERRR');
-
-            console.log(user);
-
-            return fetch(user.slack, {
-                method: 'POST',
-                body: JSON.stringify({
-                    text: `new pull request to be reviewed!!! PR: ${PR.title}`
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(() => {
-                    console.log('SEND SLACK');
-                })
-                .catch(err => {
-                    console.log('ERROR: SLACK NOT SENT');
-                    console.log(err);
-                });
+        .catch(err => {
+            console.log('ERROR: SLACK NOT SENT');
+            console.log(err);
         });
 };
 

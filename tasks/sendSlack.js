799ae4b5c57f5a0139165fcf08997f46408ed94f
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-31 00:06:28 +0200

    mongoDB

--- tasks/sendSlack.js
+++ tasks/sendSlack.js
@@ -1,21 +1,35 @@
 const fetch = require('node-fetch');
+const db = require('monk')(
+    `mongodb://dbreadwrite:${
+        process.env.MONGO_PW
+    }@ds139992.mlab.com:39992/gitbot`
+);
+const users = db.get('users');
 
 const sendSlack = PR => {
     //GET SLACKURL FROM PR
-    return fetch('https://hooks.slack.com/services/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', {
-        method: 'POST',
-        body: JSON.stringify({text: `new pull request to be reviewed! PR: ${PR.title}`}),
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + process.env.GITHUBTOKEN
-        }
-    })
-        .then(() => {
-            console.log('SEND SLACK');
+
+    users
+        .find({
+            email: 'thomas.maclean@marlon.be'
         })
-        .catch(err => {
-            console.log('ERROR: SLACK NOT SENT');
-            console.log(err);
+        .then(user => {
+            return fetch(user.slack, {
+                method: 'POST',
+                body: JSON.stringify({
+                    text: `new pull request to be reviewed!!! PR: ${PR.title}`
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(() => {
+                    console.log('SEND SLACK');
+                })
+                .catch(err => {
+                    console.log('ERROR: SLACK NOT SENT');
+                    console.log(err);
+                });
         });
 };
 

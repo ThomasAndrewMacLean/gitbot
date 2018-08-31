fe208a4e787ced448fd933d992547f6c7c427e73
Thomas MacLean <thomas.maclean@gmail.com>
2018-08-31 09:10:53 +0200

    Update sendSlack2.js

--- tasks/sendSlack2.js
+++ tasks/sendSlack2.js
@@ -4,14 +4,13 @@ const db = require('monk')(
         process.env.MONGO_PW
     }@ds139992.mlab.com:39992/gitbot`
 );
-const users = db.get('users');
 
 const sendSlack = PR => {
     //GET SLACKURL FROM PR
-    return users
+    return db.get('users')
         .find({ email: 'thomas.maclean@marlon.be' })
-        .each((user, { close }) => {
-            return fetch(
+        .then(user => {
+             fetch(
                 'https://hooks.slack.com/services/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',
                 {
                     method: 'POST',
@@ -25,12 +24,10 @@ const sendSlack = PR => {
             )
                 .then(() => {
                     console.log('SEND SLACK');
-                    close();
                 })
                 .catch(err => {
                     console.log('ERROR: SLACK NOT SENT');
                     console.log(err);
-                    close();
                 });
         });
 };
